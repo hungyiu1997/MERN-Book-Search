36330ef3f09234bbadf21565b94ef3e79393886c
Hungyiu Wong <hungyiuwong@Hungyius-MacBook-Air.local>
2021-10-07 16:08:56 -0400

    filled typeDefs.js

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,39 @@
+// structure the data and where you write your mutation and queries in the backend 
+
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql `
+    type Book {
+        bookId: ID
+        name: String
+        authors: [String]
+        description: String
+        image: String
+        link: String
+        title: String
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        savedBook: [Book]
+    }
+
+    type Query {
+        user: User
+    }
+
+    type Auth {
+        token: ID
+        user: User
+    }
+
+    type Mutation {
+        createUser(username: String, email: String, password: String): Auth
+        saveBook(bookId: ID, name: String, authors: [String], description: String, image: String, link: String, title: String): User 
+        login(username: String, password: String): Auth
+        deleteBook(bookId: ID, name: String, authors: [String], description: String, image: String, link: String, title: String): User 
+    }
+`;
