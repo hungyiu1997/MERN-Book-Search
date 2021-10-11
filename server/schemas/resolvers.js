dd1539f7d23caa6b3e2fcf06d695d60cc572d143
Hungyiu Wong <hungyiuwong@Hungyius-MacBook-Air.local>
2021-10-11 15:14:03 -0400

    fixing resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,44 +1,40 @@
 const { User } = require("../models");
-const { signToken } = require('./utils/auth');
-const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require("./utils/auth");
+const { AuthenticationError } = require("apollo-server-express");
 
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user._id) {
         const userData = await User.findOne({
-          _id: context.user._id 
+          _id: context.user._id,
           // .select can filter what you dont want to return
-          // -password: dont need password
-        }).select('-__v -password');
+          // -password: dont need password //mongoDB internal version key
+        }).select("-__v -password");
         return userData;
       }
-      
-      throw new AuthenticationError('User Not Valid!')
-    }
+
+      throw new AuthenticationError("User Not Valid!");
+    },
   },
   Mutation: {
     //arg is the req.body
     async createUser(parent, args, context) {
       const user = await User.create(args);
 
-      // if (!user) {
-      //   throw new AuthenticationError("User not valid!");
-      // }
       const token = signToken(user);
       return { token, user };
     },
     // login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
     // {body} is destructured req.body
-    async login(parent, args, context) {
-      const user = await User.findOne({
-        $or: [{ username: args.username }, { email: args.email }],
-      });
+    login: async function (parent, { email, password }) {
+      const user = await User.findOne({ email });
+      //   $or: [{ username: args.username }, { email: args.email }],
+      // });
       if (!user) {
         throw new AuthenticationError("Login incorrect!");
       }
-
-      const correctPw = await user.isCorrectPassword(args.password);
+      const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
         throw new AuthenticationError("Password incorect!");
